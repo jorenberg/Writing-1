2a2e7ed3e1e1312fc0f4d3ac263b93f79e4d0c3f
Prabhat Kumar <iamprabhat@users.noreply.github.com>
2016-09-16 00:42:30 +0530

    Initial commit
    Everest, by Prabhat Kumar, http://prabhatkumar.org/.

---
+++ source/core/inc/extras.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Custom functions that act independently of the theme templates of Everest.
+ * Eventually, some of the functionality here could be replaced by core features.
+ *  ,-.
+ *  ,     ,-.   ,-.
+ * / \   (   )-(   )
+ * \ |  ,.>-( * )-<
+ *  \|,' (   )-(   )
+ *   Y ___`-'   `-'
+ *   |/__/   `-'
+ *   |
+ *   |
+ *   |  -~PRABHAT~-
+ * __|_____________
+ *
+ * @package Everest - by Prabhat Kumar, http://prabhatkumar.org/.
+ */
