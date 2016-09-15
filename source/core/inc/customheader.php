b3eabc991f8833d1ee314423b77f7076f3438832
Prabhat Kumar <iamprabhat@users.noreply.github.com>
2016-09-16 00:36:20 +0530

    Initial commit
    Everest, by Prabhat Kumar, http://prabhatkumar.org/.

---
+++ source/core/inc/customheader.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Setup the WordPress core custom header feature.
+ *
+ * @uses syntax_header_style()
+ * @uses syntax_admin_header_style()
+ * @uses syntax_admin_header_image()
+ *
+ *  ,-.
+ *  ,     ,-.   ,-.
+ * / \   (   )-(   )
+ * \ |  ,.>-( * )-<
+ *  \|,' (   )-(   )
+ *   Y ___`-'   `-'
+ *   |/__/   `-'
+ *   |
+ *   |
+ *   |  -~PRABHAT~-
+ * __|_____________
+ *
+ * @package Everest - by Prabhat Kumar, http://prabhatkumar.org/.
+ */
