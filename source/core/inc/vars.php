809496d1269ff664147472b9fde4ee2f2e8c3376
Prabhat Kumar <iamprabhat@users.noreply.github.com>
2016-09-15 23:14:19 +0530

    Initial commit
    Everest, by Prabhat Kumar, http://prabhatkumar.org/.

---
+++ source/core/inc/vars.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Creates common globals for the rest of Everest.
+ *
+ * Simple browser detection.
+ * -- mobile devices (all Android, BlackBerry, iPhone, iPad, etc.)
+ * -- all tablets.
+ *  ,-.
+ *  ,     ,-.   ,-.
+ * / \   (   )-(   )
+ * \ |  ,.>-( * )-<
+ *  \|,' (   )-(   )
+ *   Y ___`-'   `-'
+ *   |/__/   `-'
+ *   |
+ *   |
+ *   |  -~PRABHAT~-
+ * __|_____________
+ *
+ * @package Everest - by Prabhat Kumar, http://prabhatkumar.org/.
+ */
