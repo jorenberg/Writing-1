def65c8fc8f96482ad316b298c4791f13a19c45e
Prabhat Kumar <iamprabhat@users.noreply.github.com>
2016-09-16 01:24:49 +0530

    Initial commit
    Everest, by Prabhat Kumar, http://prabhatkumar.org/.

---
+++ source/core/inc/templatetags.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * The Template Tags of Everest.
+ *
+ *  ,-.
+ *  ,     ,-.   ,-.
+ * / \   (   )-(   )
+ * \ |  ,.>-( * )-<
+ *  \|,' (   )-(   )
+ *   Y ___`-'   `-'
+ *   |/__/   `-'
+ *   |
+ *   |
+ *   |  -~PRABHAT~-
+ * __|_____________
+ *
+ * @package Everest - by Prabhat Kumar, http://prabhatkumar.org/.
+ */
