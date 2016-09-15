c6939e1a5c8586e9a7f4a226dda3a320cd462d10
Prabhat Kumar <iamprabhat@users.noreply.github.com>
2016-09-16 00:06:39 +0530

    Initial commit
    Everest, by Prabhat Kumar, http://prabhatkumar.org/.

---
+++ source/core/inc/meta.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * The Meta of Everest.
+ *
+ *  ,-.
+ *  ,     ,-.   ,-.
+ * / \   (   )-(   )
+ * \ |  ,.>-( * )-<
+ *  \|,' (   )-(   )
+ *   Y ___`-'   `-'
+ *   |/__/   `-'
+ *   |
+ *   |
+ *   |  -~PRABHAT~-
+ * __|_____________
+ *
+ * @package Everest - by Prabhat Kumar, http://prabhatkumar.org/.
+ */
