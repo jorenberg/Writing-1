2892982902cbb35c3b0bf3637a0e94d0e54061bc
Prabhat Kumar <iamprabhat@users.noreply.github.com>
2016-09-16 20:32:07 +0530

    Initial commit
    Everest, by Prabhat Kumar, http://prabhatkumar.org/.

---
+++ source/core/inc/components/sidebar.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * The sidebar containing the main widget area - of Everest.
+ *
+ *  ,-.
+ *  ,     ,-.   ,-.
+ * / \   (   )-(   )
+ * \ |  ,.>-( * )-<
+ *  \|,' (   )-(   )
+ *   Y ___`-'   `-'
+ *   |/__/   `-'
+ *   |
+ *   |
+ *   |  -~PRABHAT~-
+ * __|_____________
+ *
+ * @package Everest - by Prabhat Kumar, http://prabhatkumar.org/.
+ */
