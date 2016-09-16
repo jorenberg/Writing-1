11a845cd064844592706c5d554e85db9c9a2ae0b
Prabhat Kumar <iamprabhat@users.noreply.github.com>
2016-09-16 20:30:40 +0530

    Initial commit
    Everest, by Prabhat Kumar, http://prabhatkumar.org/.

---
+++ source/core/inc/components/search.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * The template for displaying search results pages - of Everest.
+ *
+ *  ,-.
+ *  ,     ,-.   ,-.
+ * / \   (   )-(   )
+ * \ |  ,.>-( * )-<
+ *  \|,' (   )-(   )
+ *   Y ___`-'   `-'
+ *   |/__/   `-'
+ *   |
+ *   |
+ *   |  -~PRABHAT~-
+ * __|_____________
+ *
+ * @package Everest - by Prabhat Kumar, http://prabhatkumar.org/.
+ */
