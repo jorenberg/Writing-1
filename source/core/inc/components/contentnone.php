692a282271e5b71c7ceb7b9168a0542cf3e31c5b
Prabhat Kumar <iamprabhat@users.noreply.github.com>
2016-09-16 20:12:00 +0530

    Initial commit
    Everest, by Prabhat Kumar, http://prabhatkumar.org/.

---
+++ source/core/inc/components/contentnone.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * The Content of Everest.
+ *
+ *  ,-.
+ *  ,     ,-.   ,-.
+ * / \   (   )-(   )
+ * \ |  ,.>-( * )-<
+ *  \|,' (   )-(   )
+ *   Y ___`-'   `-'
+ *   |/__/   `-'
+ *   |
+ *   |
+ *   |  -~PRABHAT~-
+ * __|_____________
+ *
+ * @package Everest - by Prabhat Kumar, http://prabhatkumar.org/.
+ */
