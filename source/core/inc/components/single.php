d5bb6d4cb394e46fc369a9389b4ce1ee9601d4f8
Prabhat Kumar <iamprabhat@users.noreply.github.com>
2016-09-16 20:34:02 +0530

    Initial commit
    Everest, by Prabhat Kumar, http://prabhatkumar.org/.

---
+++ source/core/inc/components/single.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * The template for displaying all single posts - of Everest.
+ *
+ *  ,-.
+ *  ,     ,-.   ,-.
+ * / \   (   )-(   )
+ * \ |  ,.>-( * )-<
+ *  \|,' (   )-(   )
+ *   Y ___`-'   `-'
+ *   |/__/   `-'
+ *   |
+ *   |
+ *   |  -~PRABHAT~-
+ * __|_____________
+ *
+ * @package Everest - by Prabhat Kumar, http://prabhatkumar.org/.
+ */
