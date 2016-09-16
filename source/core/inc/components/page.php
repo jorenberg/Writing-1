6fe7f4cf7f65671194aacd6af0e0fb404b76b8de
Prabhat Kumar <iamprabhat@users.noreply.github.com>
2016-09-16 20:19:10 +0530

    Initial commit
    Everest, by Prabhat Kumar, http://prabhatkumar.org/.

---
+++ source/core/inc/components/page.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * The template for displaying all pages - of Everest.
+ *
+ *  ,-.
+ *  ,     ,-.   ,-.
+ * / \   (   )-(   )
+ * \ |  ,.>-( * )-<
+ *  \|,' (   )-(   )
+ *   Y ___`-'   `-'
+ *   |/__/   `-'
+ *   |
+ *   |
+ *   |  -~PRABHAT~-
+ * __|_____________
+ *
+ * @package Everest - by Prabhat Kumar, http://prabhatkumar.org/.
+ */
