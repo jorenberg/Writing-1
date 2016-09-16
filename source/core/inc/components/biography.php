b9d8965e2bfeed1603f977f1f8bc7c525b7edfbc
Prabhat Kumar <iamprabhat@users.noreply.github.com>
2016-09-16 20:04:09 +0530

    Initial commit
    Everest, by Prabhat Kumar, http://prabhatkumar.org/.

---
+++ source/core/inc/components/biography.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * The Biography of Everest.
+ *
+ *  ,-.
+ *  ,     ,-.   ,-.
+ * / \   (   )-(   )
+ * \ |  ,.>-( * )-<
+ *  \|,' (   )-(   )
+ *   Y ___`-'   `-'
+ *   |/__/   `-'
+ *   |
+ *   |
+ *   |  -~PRABHAT~-
+ * __|_____________
+ *
+ * @package Everest - by Prabhat Kumar, http://prabhatkumar.org/.
+ */
